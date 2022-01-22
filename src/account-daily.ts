9112425b901482a618bacb60404f23aae618d3c2
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-01-22 15:39:34 +0300

    solves the task

--- src/account-daily.ts
+++ src/account-daily.ts
@@ -1,16 +1,51 @@
 import { AccountDaily } from './i-faces';
 
 interface GeneralLedgerEntryPart {
-  generalLedgerId: number;
   posted: string;
   debitAccountId: string;
   creditAccountId: string;
   amount: number;
-  referenceId: string;
 }
 
-type AccountDailyReducer = (accountId: string, ledger:GeneralLedgerEntryPart[])=>AccountDaily[];
+type AccountDailyReducer = (accountId: string, ledger: GeneralLedgerEntryPart[]) => AccountDaily[];
 
-export const accountDaily:AccountDailyReducer = ()=>{
-  throw new Error('reduces general ledger entries to daily balances');
+const createAccountDaily = (date:string):AccountDaily=>({
+  creditDayTotal: 0,
+  date,
+  debitDayTotal: 0,
+});
+const setAndReturn = (accumulator:Map<string,AccountDaily>, row:AccountDaily)=>{
+  accumulator.set(row.date,row);
+  return row;
 };
+const getDate = (dateTime: string) => dateTime.substring(0, 10);
+
+const getOrCreateDaily = (accumulator: Map<string, AccountDaily>, dateFull: string): AccountDaily => {
+  const date = getDate(dateFull);
+  const stored = accumulator.get(date);
+  return typeof stored !== 'undefined'? stored: setAndReturn(accumulator, createAccountDaily(date));
+};
+
+const createReducer = (accountId: string) => {
+  const debitFilter = (item: GeneralLedgerEntryPart) => accountId === item.debitAccountId;
+  const creditFilter = (item: GeneralLedgerEntryPart) => accountId === item.creditAccountId;
+  return (accumulator: Map<string, AccountDaily>, item: GeneralLedgerEntryPart) => {
+    const isCredit = creditFilter(item);
+    const isDebit = debitFilter(item);
+    if(isCredit || isDebit){
+      const row = getOrCreateDaily(accumulator, item.posted);
+      row.creditDayTotal += isCredit ? item.amount : 0;
+      row.debitDayTotal += isDebit ? item.amount : 0;
+    }
+    return accumulator;
+  };
+};
+
+export const accountDaily: AccountDailyReducer = (accountId, ledger) => [
+  ...ledger
+    .reduce(
+      createReducer(accountId),
+      new Map(),
+    )
+    .values()
+];
--- src/i-faces.ts
+++ src/i-faces.ts
@@ -17,7 +17,6 @@ export interface AccountEntry {
 }
 
 export interface AccountDaily {
-  accountId:number;
   date: string;
   debitDayTotal: number;
   creditDayTotal: number;
